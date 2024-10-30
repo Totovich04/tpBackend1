fda04ee2face11d0f98d7c4ec5575e7827ea5dfc
tomas <tomasolmos04@gmail.com>
2024-10-29 21:46:11 -0300

    Initial commit

---
+++ agencia/src/test/java/utn/frc/bda/agencia/AgenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.bda.agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notificacion/src/test/java/utn/frc/bda/notificacion/NotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.bda.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
