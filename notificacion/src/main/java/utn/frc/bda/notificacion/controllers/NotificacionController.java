d7e0c7d5a5c2e3d2d4d80da0a3923ca55b781852
Agustin Santiago Rey Laje <95535@sistemas.frc.edu.ar>
2024-10-30 22:08:54 -0300

    commit notificaciones

--- notificacion/src/main/java/utn/frc/bda/notificacion/controllers/NotificacionController.java
+++ notificacion/src/main/java/utn/frc/bda/notificacion/controllers/NotificacionController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notificaciones")
 public class NotificacionController {
 
-    private final NotificacionService notificacionService;
+//    private final NotificacionService notificacionService;
 }
